ac2d2ab90ad13daabb45cd194da5e57cd22b0814
N. Muskan <mmuskann2004@gmail.com>
2026-06-07 13:29:00 -0400

    Added Location Links

--- src/App.jsx
+++ src/App.jsx
@@ -12,60 +12,70 @@ const App = () => {
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Global_Citizen_Festival_Central_Park_New_York_City_from_NYonAir_%2815351915006%29.jpg/330px-Global_Citizen_Festival_Central_Park_New_York_City_from_NYonAir_%2815351915006%29.jpg" 
           name="Central Park" 
           borough="Manhattan" 
+          location="https://maps.app.goo.gl/ZP8CS7UMAKpCXjQcA"
         />
 
         <PlaceCard 
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Brooklyn_Bridge_Manhattan.jpg/330px-Brooklyn_Bridge_Manhattan.jpg" 
           name="Brooklyn Bridge" 
           borough="Brooklyn" 
+          location="https://maps.app.goo.gl/vCW3mvDFHxH8dWPV8"
         />
 
         <PlaceCard 
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Front_view_of_Statue_of_Liberty_%28cropped%29.jpg/330px-Front_view_of_Statue_of_Liberty_%28cropped%29.jpg" 
           name="Statue of Liberty" 
           borough="Manhattan" 
+          location="https://maps.app.goo.gl/Wz49pvZmCh98a78x7"
         />
 
         <PlaceCard 
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Sunset_on_the_boardwalk_%2848067690428%29.jpg/330px-Sunset_on_the_boardwalk_%2848067690428%29.jpg" 
           name="Rockaway Beach" 
           borough="Queens" 
+          location="https://maps.app.goo.gl/rhRZ7USoXMKi6Zeu7"
         />
 
         <PlaceCard 
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Coney_Island_beach_and_amusement_parks_%28June_2016%29.jpg/330px-Coney_Island_beach_and_amusement_parks_%28June_2016%29.jpg" 
           name="Coney Island" 
           borough="Brooklyn" 
+          location="https://maps.app.goo.gl/ny1DFuVhczXJLneb9"
         />
 
         <PlaceCard
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/QFM_farmhouse_jeh.jpg/330px-QFM_farmhouse_jeh.jpg"
           name="Queens County Farm Museum"
           borough="Queens"
+          location="https://maps.app.goo.gl/esLUykthsuNe8w7W6"
         />
 
         <PlaceCard
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Flushing_Meadows%E2%80%93Corona_Park.jpg/330px-Flushing_Meadows%E2%80%93Corona_Park.jpg"
           name="Flushing Meadows Corona Park"
           borough="Queens"
+          location="https://maps.app.goo.gl/5pNDDCwESu4rkSi96"
         />
 
         <PlaceCard
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bronx_Zoo_001.jpg/330px-Bronx_Zoo_001.jpg"
           name="Bronx Zoo"
           borough="Bronx"
+          location="https://maps.app.goo.gl/X4g2QKwZuuKcMv5w8"
         />
 
         <PlaceCard
           image="https://upload.wikimedia.org/wikipedia/commons/8/8a/The_New_York_Botanical_Garden_Visitor_Center.jpg"
           name="New York Botanical Garden"
           borough="Bronx"
+          location="https://maps.app.goo.gl/HqAkyobFYD8ph6Sv9"
         />
 
         <PlaceCard
           image="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Hudson_Yards_Plaza_March_2019_53.jpg/330px-Hudson_Yards_Plaza_March_2019_53.jpg"
           name="Vessel"
           borough="Manhattan"
+          location="https://maps.app.goo.gl/auLCqTCL159MJFMb9"
         />
       </div>
     </div>
--- src/components/PlaceCard.jsx
+++ src/components/PlaceCard.jsx
@@ -1,12 +1,16 @@
-const PlaceCard = ({ image, name, borough }) => {
-    return (
-        <div className="place-card">
-            <img src={image} alt={name} />
-            <h2>{name}</h2>
-            <p>{borough}</p>
-            <button>View Details</button>
-        </div>
-    );
+const PlaceCard = ({ image, name, borough, location }) => {
+  return (
+    <div className="place-card">
+      <img src={image} alt={name} />
+
+      <h2>{name}</h2>
+      <h3>{borough}</h3>
+
+      <a href={location} target="_blank" rel="noreferrer">
+        Location
+      </a>
+    </div>
+  );
 };
 
 export default PlaceCard;
