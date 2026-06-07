85dd368d433368dfae53b971bae87e4995be2dcd
N. Muskan <mmuskann2004@gmail.com>
2026-06-07 13:05:43 -0400

    Added Place Cards In App

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
+import PlaceCard from './components/PlaceCard';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="App">
+      <h1>Summer in NYC</h1>
+      <h2>Discover Spots To Visit In New York City During The Summer.</h2>
 
-      <div className="ticks"></div>
+      <div className="place-cards-container">
+        <PlaceCard 
+          image="https://example.com/image1.jpg" 
+          name="Central Park" 
+          borough="Manhattan" 
+          />
+        <PlaceCard 
+          image="https://example.com/image2.jpg" 
+          name="Brooklyn Bridge" 
+          borough="Brooklyn" 
+          />
+        <PlaceCard 
+          image="https://example.com/image3.jpg" 
+          name="Statue of Liberty" 
+          borough="Manhattan" 
+          />
+        <PlaceCard 
+          image="https://example.com/image3.jpg" 
+          name="Rockaway Beach" 
+          borough="Queens" 
+          />
+        <PlaceCard 
+          image="https://example.com/image3.jpg" 
+          name="Coney Island" 
+          borough="Brooklyn" 
+          />
+        <PlaceCard
+          image="https://example.com/image3.jpg"
+          name="Queens County Farm Museum"
+          borough="Queens"
+         />
+        <PlaceCard
+          image="https://example.com/image3.jpg"
+          name="Flushing Meadows Corona Park"
+          borough="Queens"
+         />
+        <PlaceCard
+          image="https://example.com/image3.jpg"
+          name="Bronx Zoo"
+          borough="Bronx"
+         />
+        <PlaceCard
+          image="https://example.com/image3.jpg"
+          name="New York Botanical Garden"
+          borough="Bronx"
+         />
+        <PlaceCard
+          image="https://example.com/image3.jpg"
+          name="Vessel"
+          borough="Manhattan"
+         />
+      </div>
+    </div>
+  );
+};
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+export default App;
